69d1265ff8fd3c201a0d175bc4d6f6dc9f63561a
Chass <mesinachass11@gmail.com>
2026-01-11 20:22:04 +0800

    Updated models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, Float, String, Date, Boolean
 from .database import Base
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -7,3 +7,20 @@ class Chart(Base):
     id = Column(Integer, primary_key=True, index=True)
     chart_name = Column(String, unique=True, index=True)
     data = Column(JSONB)
+
+class SpotifyData(Base):
+    __tablename__ = "spotify_data"
+    id = Column(Integer, primary_key=True, index=True)
+    spotify_id = Column(Integer, index=True)
+    name = Column(String, index=True)
+    artists = Column(String, index=True)
+    country = Column(String, index=True)
+    snapshot_date = Column(Date, index = True)
+    daily_rank = Column(Integer, index=True)
+    daily_movement = Column(Integer, index=True)
+    duration_ms = Column(Integer)
+    explicit = Column(Boolean)
+    danceability = Column(Float, index=True)
+    energy = Column(Float, index=True)
+    valence = Column(Float, index=True)
+    tempo = Column(Float, index=True)
